a9b3140fc480fff9222babee85aa5af00293d47e
ojmassam <ojmassam@gmail.com>
2018-05-08 22:36:05 +0200

    Added Database Manager class and added each table as a class with their table names and column names

---
+++ app/src/main/java/za/ac/uj/eve/dynamicwealthassistant/DB_Category.java
@@ -0,0 +1,9 @@
+package za.ac.uj.eve.dynamicwealthassistant;
+
+public class DB_Category {
+    public static final String TABLE_NAME = "Category";
+
+    public static final String COL_1 = "categoryID";
+    public static final String COL_2 = "categoryName";
+    public static final String COL_3 = "icon";
+}
---
+++ app/src/main/java/za/ac/uj/eve/dynamicwealthassistant/DB_User.java
@@ -0,0 +1,9 @@
+package za.ac.uj.eve.dynamicwealthassistant;
+
+public class DB_User {
+    public static final String TABLE_NAME = "User";
+
+    public static final String COL_1 = "userID";
+    public static final String COL_2 = "username";
+    public static final String COL_3 = "password";
+}
